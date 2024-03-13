3fee27b19630afb66ef61655895510542b5b469b
dusty.shaw <dusty.shaw@ENGRJJCP5S3>
2024-03-13 17:30:21 -0600

    moved to my own repo

---
+++ MobileApp/TicketWebApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using TicketClassLib.Services;
+using TicketWebApp.Components;
+using TicketWebApp.Data;
+using TicketWebApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Added logging for otel 
+builder.Services.AddLogging();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddSingleton<ITicketService, ApiTicketService>();
+builder.Services.AddSingleton<IEventService, ApiEventService>();
+builder.Services.AddDbContextFactory<PostgresContext>(optionsBuilder => optionsBuilder.UseNpgsql("Name=TicketsDB"));
+builder.Services.AddScoped<EmailSender>();
+
+builder.Services.AddHealthChecks();
+
+// Add add builder.logging.addopentelem for logging specifically
+const string serviceName = "dustys-service";
+
+builder.Logging.AddOpenTelemetry(options =>
+{
+    options
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName))
+        .AddOtlpExporter(opt =>
+            {
+                opt.Endpoint = new Uri("http://otel-collector:4317"); // not sure what url this is
+            })
+        .AddConsoleExporter(); // optional
+});
+// Add logging.addtelemetry Opentelemetry.io > Language APIS and SDKs > .NET (website)
+// inside adding open telemetry, add oltp exporter with grpc endpoint found in docker-compose
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+
+}
+
+app.MapHealthChecks("/healthCheck", new HealthCheckOptions
+{
+    AllowCachingResponses = false,
+    ResultStatusCodes =
+                {
+                    [HealthStatus.Healthy] = StatusCodes.Status200OK,
+                    [HealthStatus.Degraded] = StatusCodes.Status200OK,
+                    [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+                }
+});
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Blazor API V1");
+});
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.MapControllers();
+app.Run();
+
+public partial class Program { }
