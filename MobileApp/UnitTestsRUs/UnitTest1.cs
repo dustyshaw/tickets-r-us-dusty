66aa470fb1495320362d2d8a60252afc877e9cdb
dusty.shaw <dusty.shaw@ENGRJJCP5S3>
2024-03-13 18:20:51 -0600

    unit tests pass

---
+++ MobileApp/UnitTestsRUs/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestsRUs;
+
+[TestFixture]
+public class UnitTest1
+{
+    [Test]
+    public void IsPrime_InputIs1_ReturnFalse()
+    {
+        var result = 1;
+
+        Assert.AreEqual(1, result);
+    }
+}
