cec233f2d1de4abfb7d1d12e6e875cee6c015911
dusty.shaw <dusty.shaw@ENGRJJCP5S3>
2024-03-13 18:01:11 -0600

    builds now!!!

--- MobileApp/TestsRUS/TicketsApiFactory.cs
+++ MobileApp/TestsRUS/TicketsApiFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Testcontainers.PostgreSql;
 using TicketWebApp.Data;
