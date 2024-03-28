9e2f691fb2b0ceff531edfdd8b731845ca334280
dusty.shaw <dusty.shaw@ENGRJJCP5S3>
2024-03-27 18:04:46 -0600

    added last dashboard

--- MobileApp/TicketWebApp/Services/ApiTicketService.cs
+++ MobileApp/TicketWebApp/Services/ApiTicketService.cs
@@ -100,7 +100,7 @@ public partial class ApiTicketService : ITicketService
         TicketsHaveChanged?.Invoke(this, new EventArgs());
 
         watch.Stop();
-        MyMetrics.EventsMetic.histogram.Record(watch.ElapsedMilliseconds);
+        MyMetrics.EventsMetic.histogram.Record(watch.Elapsed.Milliseconds);
 
         return TicketStatus.Success;
     }
